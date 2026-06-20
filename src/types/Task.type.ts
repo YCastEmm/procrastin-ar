e9c58871020d7e16f9804f83b42955262fb3441b
Yair Castagnola <ycastagnola@x-28.com>
2026-06-19 21:48:28 -0300

    feat: extiende la interfaz de Task

--- src/types/Task.type.ts
+++ src/types/Task.type.ts
@@ -2,7 +2,18 @@
 
 export interface Task {
     id: string
-    fecha: string,
-    descripcion: string,
-    completada: boolean, 
+    fecha: string
+    descripcion: string
+    completada: boolean
+    fotoUri?: string
+    ubicacion?: {
+        lat: number
+        lng: number
+        direccion?: string
+    }
+    contacto?: {
+        nombre: string
+        telefono?: string
+    }
+    calendarEventId?: string
 }
